1509affd68fa7ad0cee04d04d61a0944148a4ad1
Alex Bostock <alex@bostock.uk>
2023-09-09 09:43:35 +0100

    Setup Prettier

--- src/lib/Board.test.ts
+++ src/lib/Board.test.ts
@@ -1,62 +1,66 @@
-import Board, { PieceColour, PieceType } from './Board';
-import { positionFromEncodedCoordinates } from './Position';
+import Board, { PieceColour, PieceType } from "./Board";
+import { positionFromEncodedCoordinates } from "./Position";
 
-describe('Initial state', () => {
-    test('white is first to move', () => {
-        const board = new Board()
-        expect(board.nextToMove).toBe('white')
-    })
+describe("Initial state", () => {
+  test("white is first to move", () => {
+    const board = new Board();
+    expect(board.nextToMove).toBe("white");
+  });
 
-    test('board is setup as standard', () => {
-        const board = new Board()
+  test("board is setup as standard", () => {
+    const board = new Board();
 
-        function expectHasPieceAtPosition(colour: PieceColour, type: PieceType, encodedCoords: string) {
-            const position = positionFromEncodedCoordinates(encodedCoords)
-            const piece = board.pieceAtPosition(position);
-            expect(piece).toEqual({
-                colour,
-                type,
-                position,
-            })
-        }
-        
-        expectHasPieceAtPosition('white', 'rook', 'A1')
-        expectHasPieceAtPosition('white', 'knight', 'B1')
-        expectHasPieceAtPosition('white', 'bishop', 'C1')
-        expectHasPieceAtPosition('white', 'queen', 'D1')
-        expectHasPieceAtPosition('white', 'king', 'E1')
-        expectHasPieceAtPosition('white', 'bishop', 'F1')
-        expectHasPieceAtPosition('white', 'knight', 'G1')
-        expectHasPieceAtPosition('white', 'rook', 'H1')
-        expectHasPieceAtPosition('white', 'pawn', 'A2')
-        expectHasPieceAtPosition('white', 'pawn', 'B2')
-        expectHasPieceAtPosition('white', 'pawn', 'C2')
-        expectHasPieceAtPosition('white', 'pawn', 'D2')
-        expectHasPieceAtPosition('white', 'pawn', 'E2')
-        expectHasPieceAtPosition('white', 'pawn', 'F2')
-        expectHasPieceAtPosition('white', 'pawn', 'G2')
-        expectHasPieceAtPosition('white', 'pawn', 'H2')
-        expectHasPieceAtPosition('black', 'rook', 'A8')
-        expectHasPieceAtPosition('black', 'knight', 'B8')
-        expectHasPieceAtPosition('black', 'bishop', 'C8')
-        expectHasPieceAtPosition('black', 'queen', 'D8')
-        expectHasPieceAtPosition('black', 'king', 'E8')
-        expectHasPieceAtPosition('black', 'bishop', 'F8')
-        expectHasPieceAtPosition('black', 'knight', 'G8')
-        expectHasPieceAtPosition('black', 'rook', 'H8')
-        expectHasPieceAtPosition('black', 'pawn', 'A7')
-        expectHasPieceAtPosition('black', 'pawn', 'B7')
-        expectHasPieceAtPosition('black', 'pawn', 'C7')
-        expectHasPieceAtPosition('black', 'pawn', 'D7')
-        expectHasPieceAtPosition('black', 'pawn', 'E7')
-        expectHasPieceAtPosition('black', 'pawn', 'F7')
-        expectHasPieceAtPosition('black', 'pawn', 'G7')
-        expectHasPieceAtPosition('black', 'pawn', 'H7')
+    function expectHasPieceAtPosition(
+      colour: PieceColour,
+      type: PieceType,
+      encodedCoords: string
+    ) {
+      const position = positionFromEncodedCoordinates(encodedCoords);
+      const piece = board.pieceAtPosition(position);
+      expect(piece).toEqual({
+        colour,
+        type,
+        position,
+      });
+    }
 
-        const sampleEmptySquares = ['A3', 'C4', 'D4','F5', 'coordsF6']
-        for (const coords of sampleEmptySquares) {
-            const position = positionFromEncodedCoordinates(coords)
-            expect(board.pieceAtPosition(position)).toBeUndefined()
-        }
-    })
-})
+    expectHasPieceAtPosition("white", "rook", "A1");
+    expectHasPieceAtPosition("white", "knight", "B1");
+    expectHasPieceAtPosition("white", "bishop", "C1");
+    expectHasPieceAtPosition("white", "queen", "D1");
+    expectHasPieceAtPosition("white", "king", "E1");
+    expectHasPieceAtPosition("white", "bishop", "F1");
+    expectHasPieceAtPosition("white", "knight", "G1");
+    expectHasPieceAtPosition("white", "rook", "H1");
+    expectHasPieceAtPosition("white", "pawn", "A2");
+    expectHasPieceAtPosition("white", "pawn", "B2");
+    expectHasPieceAtPosition("white", "pawn", "C2");
+    expectHasPieceAtPosition("white", "pawn", "D2");
+    expectHasPieceAtPosition("white", "pawn", "E2");
+    expectHasPieceAtPosition("white", "pawn", "F2");
+    expectHasPieceAtPosition("white", "pawn", "G2");
+    expectHasPieceAtPosition("white", "pawn", "H2");
+    expectHasPieceAtPosition("black", "rook", "A8");
+    expectHasPieceAtPosition("black", "knight", "B8");
+    expectHasPieceAtPosition("black", "bishop", "C8");
+    expectHasPieceAtPosition("black", "queen", "D8");
+    expectHasPieceAtPosition("black", "king", "E8");
+    expectHasPieceAtPosition("black", "bishop", "F8");
+    expectHasPieceAtPosition("black", "knight", "G8");
+    expectHasPieceAtPosition("black", "rook", "H8");
+    expectHasPieceAtPosition("black", "pawn", "A7");
+    expectHasPieceAtPosition("black", "pawn", "B7");
+    expectHasPieceAtPosition("black", "pawn", "C7");
+    expectHasPieceAtPosition("black", "pawn", "D7");
+    expectHasPieceAtPosition("black", "pawn", "E7");
+    expectHasPieceAtPosition("black", "pawn", "F7");
+    expectHasPieceAtPosition("black", "pawn", "G7");
+    expectHasPieceAtPosition("black", "pawn", "H7");
+
+    const sampleEmptySquares = ["A3", "C4", "D4", "F5", "F6"];
+    for (const coords of sampleEmptySquares) {
+      const position = positionFromEncodedCoordinates(coords);
+      expect(board.pieceAtPosition(position)).toBeUndefined();
+    }
+  });
+});
--- src/lib/Position.ts
+++ src/lib/Position.ts
@@ -1,57 +1,64 @@
-type RowOrFileNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7
-export const allRowsOrFiles: RowOrFileNumber[] = [0, 1, 2, 3, 4, 5, 6, 7]
+type RowOrFileNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+export const allRowsOrFiles: RowOrFileNumber[] = [0, 1, 2, 3, 4, 5, 6, 7];
 
 const fileMapping: Record<string, RowOrFileNumber> = {
-    'A': 0,
-    'B': 1,
-    'C': 2,
-    'D': 3,
-    'E': 4,
-    'F': 5,
-    'G': 6,
-    'H': 7,
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+  F: 5,
+  G: 6,
+  H: 7,
 };
 
 const rowMapping: Record<string, RowOrFileNumber> = {
-    '1': 0,
-    '2': 1,
-    '3': 2,
-    '4': 3,
-    '5': 4,
-    '6': 5,
-    '7': 6,
-    '8': 7,
-}
+  "1": 0,
+  "2": 1,
+  "3": 2,
+  "4": 3,
+  "5": 4,
+  "6": 5,
+  "7": 6,
+  "8": 7,
+};
 
 export default class Position {
-    readonly x: RowOrFileNumber
-    readonly y: RowOrFileNumber
+  readonly x: RowOrFileNumber;
+  readonly y: RowOrFileNumber;
 
-    constructor(x: RowOrFileNumber, y: RowOrFileNumber) {
-        this.x = x
-        this.y = y
-    }
+  constructor(x: RowOrFileNumber, y: RowOrFileNumber) {
+    this.x = x;
+    this.y = y;
+  }
 
-    get encodedCoordinate(): string {
-        const file = Object.entries(fileMapping).find(([_, index]) => index === this.x)
-        const row = Object.entries(rowMapping).find(([_, index]) => index === this.y)
-        if (file === undefined || row === undefined) {
-            throw new Error('Unexpected internal error')
-        }
-        return `${file[0]}${row[0]}`
+  get encodedCoordinate(): string {
+    const file = Object.entries(fileMapping).find(
+      ([_, index]) => index === this.x
+    );
+    const row = Object.entries(rowMapping).find(
+      ([_, index]) => index === this.y
+    );
+    if (file === undefined || row === undefined) {
+      throw new Error("Unexpected internal error");
     }
+    return `${file[0]}${row[0]}`;
+  }
 }
 
-export function positionFromEncodedCoordinates(encodedCoordinates: string): Position {
-    const coordinateComponents = encodedCoordinates.split('')
-    if (coordinateComponents.length !== 2) {
-        throw new Error(`Invalid coordinates: ${encodedCoordinates}`)
-    }
-    const [file, row] = coordinateComponents;
-    const fileNumber: RowOrFileNumber | undefined = fileMapping[file.toUpperCase()]
-    const rowNumber: RowOrFileNumber | undefined = rowMapping[row]
-    if (fileNumber === undefined || rowNumber === undefined) {
-        throw new Error(`Invalid coordinates: ${encodedCoordinates}`)
-    }
-    return new Position(fileNumber, rowNumber)
+export function positionFromEncodedCoordinates(
+  encodedCoordinates: string
+): Position {
+  const coordinateComponents = encodedCoordinates.split("");
+  if (coordinateComponents.length !== 2) {
+    throw new Error(`Invalid coordinates: ${encodedCoordinates}`);
+  }
+  const [file, row] = coordinateComponents;
+  const fileNumber: RowOrFileNumber | undefined =
+    fileMapping[file.toUpperCase()];
+  const rowNumber: RowOrFileNumber | undefined = rowMapping[row];
+  if (fileNumber === undefined || rowNumber === undefined) {
+    throw new Error(`Invalid coordinates: ${encodedCoordinates}`);
+  }
+  return new Position(fileNumber, rowNumber);
 }
