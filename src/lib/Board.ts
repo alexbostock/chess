6363c8f215d072b12de3e9d42d447c9a108017f8
Alex Bostock <alex@bostock.uk>
2023-09-09 09:39:08 +0100

    Build base data structures

---
+++ src/lib/Board.ts
@@ -0,0 +1,61 @@
+import Position, { allRowsOrFiles } from "./Position"
+
+export type PieceColour = 'white' | 'black'
+export type PieceType = 'pawn' | 'bishop' | 'knight' | 'rook' | 'queen' | 'king'
+
+interface Piece {
+    colour: PieceColour
+    type: PieceType
+    position: Position
+}
+
+function createDefaultPieces(): Piece[] {
+    const whitePawns: Piece[] = allRowsOrFiles.map((x) => ({
+        colour: 'white',
+        type: 'pawn',
+        position: new Position(x, 1)
+    }))
+    const blackPawns: Piece[] = allRowsOrFiles.map((x) => ({
+        colour: 'black',
+        type: 'pawn',
+        position: new Position(x, 6)
+    }))
+    return [
+        ...whitePawns,
+        ...blackPawns,
+        { position: new Position(0, 0), colour: 'white', type: 'rook', },
+        { position: new Position(0, 7), colour: 'black', type: 'rook', },
+        { position: new Position(1, 0), colour: 'white', type: 'knight', },
+        { position: new Position(1, 7), colour: 'black', type: 'knight', },
+        { position: new Position(2, 0), colour: 'white', type: 'bishop', },
+        { position: new Position(2, 7), colour: 'black', type: 'bishop', },
+        { position: new Position(3, 0), colour: 'white', type: 'queen', },
+        { position: new Position(3, 7), colour: 'black', type: 'queen', },
+        { position: new Position(4, 0), colour: 'white', type: 'king', },
+        { position: new Position(4, 7), colour: 'black', type: 'king', },
+        { position: new Position(5, 0), colour: 'white', type: 'bishop', },
+        { position: new Position(5, 7), colour: 'black', type: 'bishop', },
+        { position: new Position(6, 0), colour: 'white', type: 'knight', },
+        { position: new Position(6, 7), colour: 'black', type: 'knight', },
+        { position: new Position(7, 0), colour: 'white', type: 'rook', },
+        { position: new Position(7, 7), colour: 'black', type: 'rook', }
+    ]
+}
+
+export default class Board {
+    private pieces: Piece[]
+    nextToMove: PieceColour
+
+    constructor() {
+        this.pieces = createDefaultPieces()
+        this.nextToMove = 'white'
+    }
+
+    pieceAtPosition(position: Position): Piece | undefined {
+        for (const piece of this.pieces) {
+            if (piece.position.x === position.x && piece.position.y === position.y) {
+                return piece
+            }
+        }
+    }
+}
---
+++ src/lib/Position.test.ts
@@ -0,0 +1,44 @@
+import Position, { positionFromEncodedCoordinates } from "./Position"
+
+describe('Position', () => {
+    it('outputs coordinates in human-readable format', () => {
+        const a1 = new Position(0, 0)
+        expect(a1.encodedCoordinate).toBe('A1')
+
+        const b3 = new Position(1, 2)
+        expect(b3.encodedCoordinate).toBe('B3')
+
+        const f8 = new Position(5, 7)
+        expect(f8.encodedCoordinate).toBe('F8')
+    })
+})
+
+describe('positionFromEncodedCoordinates', () => {
+    it('instantiates Position from human readable coordinates', () => {
+        const c3 = positionFromEncodedCoordinates('C3')
+        expect(c3.encodedCoordinate).toBe('C3')
+    })
+
+    it('accepts lower case coordinates', () => {
+        const e6 = positionFromEncodedCoordinates('e6')
+        expect(e6.encodedCoordinate).toBe('E6')
+    })
+
+    it('throws on out-of-bounds input', () => {
+        expect(() => positionFromEncodedCoordinates('i1')).toThrow()
+
+        expect(() => positionFromEncodedCoordinates('a0')).toThrow()
+
+        expect(() => positionFromEncodedCoordinates('e9')).toThrow()
+    })
+
+    it('throws on invalid input format', () => {
+        expect(() => positionFromEncodedCoordinates('')).toThrow()
+
+        expect(() => positionFromEncodedCoordinates('a')).toThrow()
+
+        expect(() => positionFromEncodedCoordinates('1b')).toThrow()
+
+        expect(() => positionFromEncodedCoordinates(('a13'))).toThrow()
+    })
+})
